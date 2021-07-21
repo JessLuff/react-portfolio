b6227b96e23466db7cb902aa5a1b0b3bffb58e15
jessluff <jessica.luff@outlook.com>
2021-07-21 20:05:40 +1000

    Readme updated

--- react-portfolio/src/components/NavTabs.js
+++ react-portfolio/src/components/NavTabs.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../styles/Navbar.css';
 
-
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -9,12 +9,10 @@ export default function Contact() {
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'name') {
       setName(inputValue);
     } else if (inputType === 'email') {
@@ -31,19 +29,14 @@ export default function Contact() {
 
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
     if (!validateEmail(email)) {
       setErrorMessage('Email is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
     alert(`Hello ${name}`);
 
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
     setName('');
     setEmail('');
     setMessage('');
